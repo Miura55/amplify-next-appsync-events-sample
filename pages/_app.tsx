4324a141842152fab9adebc7c27e6cc6cdb102a6
Miura55 <acordion.piano@gmail.com>
2024-12-17 00:58:05 +0900

    Amplifyの設定を更新し、APIのイベントエンドポイントと認証モードを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,19 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(outputs);
+Amplify.configure({
+  ...outputs,
+  API: {
+    ...outputs.API,
+    Events: {
+      ...outputs.API.Events,
+      endpoint: outputs.API.Events.endpoint,
+      region: outputs.API.Events.region,
+      defaultAuthMode: outputs.API.Events.defaultAuthMode,
+      apiKey: outputs.API.Events.apiKey,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
